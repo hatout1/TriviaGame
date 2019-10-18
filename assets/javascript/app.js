e3ae5a5770b95030f06a3addb09e1c8637744705
hatout01 <hatout1@gmail.com>
2019-10-17 18:01:43 -0700

    fixed a function that hides questions ans show result display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,25 +6,31 @@ $(document).ready(function () {
     // var Unanswered = []
 
 
-
-    $('.btn1').click(function () {
-        $(this).siblings(".item").toggle();
-        $(this).remove(".btn1").toggle();
-
-
-    });
+    // function to remove welcome text and start button.
     $('.btn1').click(function myFunction() {
-
-
+        $(this).siblings(".item").toggle();
+        $(this).remove(".btn1");
         var x = document.getElementById("wel");
         if (x.style.display === "none") {
             x.style.display = "block";
         } else {
             x.style.display = "none";
         }
-
-
     });
+
+    // function to removequestions and timer then show result layout.
+    $('.btn2').click(function myFunction2() {
+        // console.log("it is working");
+        $(this).next().toggle();
+        $(this).remove(".btn2");
+        var V = document.getElementById("form");
+        if (V.style.display === "none") {
+            V.style.display = "block";
+        } else {
+            V.style.display = "none";
+        }
+
+    })
 
 })
 
