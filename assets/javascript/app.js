05e305df3988351cdf3b5c4c42f3e1909309d402
hatout01 <hatout1@gmail.com>
2019-10-17 19:48:32 -0700

    set the timer function which will move to next steep if either submit button clicked or countdown =0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     // var IncorrectAnswers = []
     // var Unanswered = []
 
+    // set this var on global to function for both cases (count down and submit button click)
+    var timeleft = 3;
 
     // function to remove welcome text and start button.
     $('.btn1').click(function myFunction() {
@@ -16,14 +18,39 @@ $(document).ready(function () {
         } else {
             x.style.display = "none";
         }
+
+        // after the above function the timer will automatically start!
+        // timer function
+        var progress = setInterval(function countDown() {
+            // to show the count down on html page
+            document.getElementById("timer1").innerHTML = timeleft-- + " Seconds left!!!";
+
+            // if countdown = 0 move to next steep
+            if (timeleft < 0) {
+
+                clearInterval(progress);
+                $(".btn2").next().toggle();
+                $(".btn2").remove(".btn2");
+                var V = document.getElementById("form");
+                if ((V.style.display === "none") && (timeleft >= 0)) {
+                    V.style.display = "block";
+                } else {
+                    V.style.display = "none";
+                }
+
+            }
+        }, 1000);
     });
 
-    // function to removequestions and timer then show result layout.
+    // function to remove questions and timer, then to show result layout.
     $('.btn2').click(function myFunction2() {
-        // console.log("it is working");
+        // on click this function will stop the timer by setting it to "0"
+        timeleft = 0;
+        //then will show results lay out after removing questioins and timer.       
         $(this).next().toggle();
         $(this).remove(".btn2");
         var V = document.getElementById("form");
+
         if (V.style.display === "none") {
             V.style.display = "block";
         } else {
@@ -33,6 +60,8 @@ $(document).ready(function () {
     })
 
 })
+
+
 
 
 
